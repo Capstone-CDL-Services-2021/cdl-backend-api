8839f34f19fce8c3337813ed6121d28aceac3992
ali6465 <45683275+ali6465@users.noreply.github.com>
2021-02-23 15:26:13 -0700

    BACKEND

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = User::factory()->count(10)->create();    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForgotController;
+use App\Http\Controllers\HelloController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('hello',[HelloController::class, 'hello']);
+Route::post('login',[AuthController::class, 'login']);
+Route::post('register',[AuthController::class,'register']);
+Route::post('forgot',[ForgotController::class, 'forgot']);
+Route::post('reset',[ForgotController::class, 'reset']);
+
+Route::get('user',[AuthController::class,'user'])->middleware('auth:api');
