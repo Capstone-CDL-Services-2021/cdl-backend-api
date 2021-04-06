dcb8d3b05dfcd913b0347570d15497e03b05092f
jobanh <jobanh@users.noreply.github.com>
2021-04-05 18:01:26 -0600

    Added Project stuff

---
+++ database/migrations/2021_04_05_000000_create_projects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('Projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('Type_Of_Service') ;
+            $table->string('Customer_Name');
+            $table->string('Customer_Email') ;
+            $table->string('Customer_Address');
+            $table->date('Date_Requested') ;
+            $table->tinyInteger('Completed');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('Projects');
+    }
+}
