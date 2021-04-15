f848dc195c9fdae7d90f3c2eea020b8121a6764f
jobanh <jobanh@users.noreply.github.com>
2021-04-14 19:44:43 -0600

    TestimonialController javadoc

--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -7,8 +7,18 @@ use App\Models\Testimonial;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * Class TestimonialController
+ * Handles the requests for adding, receiving and toggling testimonials
+ * @package App\Http\Controllers
+ */
 class TestimonialController extends Controller
 {
+    /**
+     * addTestimonial - This method commits to the database and adds a new testimonial to the testimonial table
+     * @param TestimonialRequest $request The parameters that are received from the front end (Title, testimonial description, rating and name)
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     public function addTestimonial(TestimonialRequest $request) {
         try {
             $testimonial = Testimonial::create([
@@ -28,10 +38,18 @@ class TestimonialController extends Controller
         }
     }
 
+    /**
+     * getAllTetimonials - This method selects from the testimonials table to retrieve the testimonials written
+     * @return Testimonial[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function getAllTestimonials(){
         return Testimonial::all();
     }
 
+    /**
+     * toggleVisibility - This method is used to toggle visibility of the testimonial to users on the testimonials front end page
+     * @param Request $request - The paramater received from the front end (testimonial ID)
+     */
     public function toggleVisibility(Request $request){
         $toggle = 0;
         if($request->input('toggleID') === 0)
