4ef9486978eaacb741d794edd611c191c37d3250
Exenpai <ju5t1n53rr4n0@gmail.com>
2021-04-14 19:44:58 -0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * Class UserController
+ * Handles requests for creation,deletion,and editing of users
+ * @package App\Http\Controllers
+ */
 class UserController extends Controller
 {
+    /**
+     * This method grabs all the users in the "users" table from the database
+     * except for admin
+     *
+     * @return Collection
+     * array of all the column values for the users
+     */
     public function getAllUsers(): Collection {
         return  DB::table('users')
             ->where('admin', '=', '0')
             ->get();
     }
 
+    /**
+     * This method updates a specific user based on the id
+     * and changes the first name, last name and email
+     * into the users table from the database
+     *
+     * @param Request $request takes in the id, first name, last name, email
+     */
     public function updateUserInfo(Request $request){
         DB::table('users')
             ->where('id', '=', $request->input('id'))
@@ -24,17 +43,26 @@ class UserController extends Controller
             ]);
     }
 
+    /**
+     * This method deletes a specific user based on the id
+     * and removes the row in the users table from the database
+     *
+     * @param Request $request takes in id
+     */
     public function deleteUser(Request $request){
         DB::table('users')
             ->where('id', '=', $request->input('id'))
             ->delete();
     }
 
-    public function updateBlocked(Request $request){
-        DB::table('users')
-            ->where('id', '=', $request->input('id'))
-            ->update(['blocked' => '1']);
-    }
+    /**
+     * This method blocks/unblocks a specific user based on the id
+     * and updates the blocked column in the users table from the database.
+     * If blocked column value of the user is 1 -> set to 0 (unblock)
+     * otherwise set to 1 (block)
+     *
+     * @param Request $request takes in id
+     */
 
     public function toggleBlocked(Request $request){
         if($request->input('blocked') === 0)
